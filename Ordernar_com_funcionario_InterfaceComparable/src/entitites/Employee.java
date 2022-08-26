1a7498a1e3f81ce3d53b7d58efed877239432ce5
Kauan-Almeida <kauanalmeida2233@gmail.com>
2022-08-26 15:26:27 -0300

    Ordenar com funcionarios

---
+++ Ordernar_com_funcionario_InterfaceComparable/src/entitites/Employee.java
@@ -0,0 +1,41 @@
+package entitites;
+
+public class Employee implements Comparable<Employee>{
+
+	private String name;
+	private Double salary;
+	
+	public Employee() {
+	}
+	
+	public Employee(String name, Double salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Double getSalary() {
+		return salary;
+	}
+	
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public int compareTo(Employee other) {
+		return name.compareTo(other.getName());
+		
+		/*to sort in salary would be the same, changing only the "name" for "salary"
+		 *return salary.compareTo(other get.Salary());  (growing)
+		 *return -salary.compareTo(other get.Salary()); (descending)
+		 */	
+	}
+}
